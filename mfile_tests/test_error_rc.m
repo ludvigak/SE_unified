e3f0b6a95315da1a2d6974f356583f2d370f0dfb
ludvigak <ludvigak@8233c644-40ad-4d59-be1a-cf114bdf719e>
2012-10-16 13:35:24 +0000

    minor changes to test_error_rc
    git-svn-id: file:///afs/nada.kth.se/home/z/u1dlwbez/svnroot/SE_Stresslet@23 8233c644-40ad-4d59-be1a-cf114bdf719e

--- mfile_tests/test_error_rc.m
+++ mfile_tests/test_error_rc.m
@@ -1,15 +1,21 @@
 clear
 
-xilist = [6 8 10 14];
+xilist = [5 7 10 15];
+N = 1000;
+% N = 5000;
+
+rc = 0.1:0.02:1;
+
+% rc = 0.1:0.1:1;
+
+box = [2 2 2];
 
 generate_ref_state = false;
 generate_ref_solutions = generate_ref_state;
 
-filename_state = 'mfile_tests/test_error_rc_ref_state.mat';
+filename_state = 'mfile_tests/refmat/test_error_rc_ref_state.mat';
 if generate_ref_state
     disp('Generating reference state...')
-    N = 3000;
-    box = [2 2 2];
     [x f nvec] = generate_state(N,box);
     idx = 1:N;
     save(filename_state,...
@@ -22,7 +28,7 @@ end
 
 if generate_ref_solutions
     for xi=xilist
-        filename_sol = sprintf('mfile_tests/test_error_rc_ref_sol_xi=%.3f.mat',xi);    
+        filename_sol = sprintf('mfile_tests/refmat/test_error_rc_ref_sol_xi=%.3f.mat',xi);    
         NOL_ref = 1;
         rc_ref = NOL_ref*max(box)*10;
         UREF = stresslet_direct_real_fast(idx, x, f, nvec, xi,  box, NOL_ref, rc_ref);
@@ -32,17 +38,16 @@ if generate_ref_solutions
     end
 end
 
-figure(1),clf
-figure(2),clf
+figure(1),clf,publication_fig
+figure(2),clf,publication_fig
 
 kvot = [];
 
 for xi=xilist
-    filename_sol = sprintf('mfile_tests/test_error_rc_ref_sol_xi=%.3f.mat',xi);  
+    filename_sol = sprintf('mfile_tests/refmat/test_error_rc_ref_sol_xi=%.3f.mat',xi);  
     load(filename_sol)
     NOL = 1;
-    rc = 0.1:0.02:1;
-
+    
     for i=1:numel(rc)
         if rc(i)>min(box)/2
             u = stresslet_direct_real_fast(idx, x, f, nvec, xi,  box, NOL, rc(i) );
@@ -54,30 +59,33 @@ for xi=xilist
     end
 
     S2 = zeros(3,3);
-    Qt = 0;
     for l=1:3
         for m=1:3
             S2(l,m) = sum( ( f(:,l).*nvec(:,m) ).^2 );
         end
     end
    
-    Q = sum(S2(:))/9;
+    Q = sum(S2(:));
     V = prod(box);
         
     % Power of Mathematica!
-    est = sqrt(Q/V * exp(-2*xi^2*rc.^2).*(...
-            109*xi^2*rc + 1588/3*rc.^3*xi^4 -464*rc.^5*xi^6 + 448/3*rc.^7*xi^8 ...
-          ) );
+    % Long polynomial form
+%     est = sqrt(Q/V/9 * exp(-2*xi^2*rc.^2).*(...
+%             109*xi^2*rc + 1588/3*rc.^3*xi^4 -464*rc.^5*xi^6 + 448/3*rc.^7*xi^8 ...
+%           ) );  
+    xirc = xi*rc;
+    est = sqrt( exp(-2*xirc.^2) * Q/V * 1/27 * xi^2 .* rc .*( ...
+                327 + 1588*xirc.^2 -1392*xirc.^4 + 448*xirc.^6) );
       
     % Short form
 %     est = 8*sqrt(7*Q/V/3*rc.^7)*xi^4.*exp(-xi^2*rc.^2);
           
-    figure(1)
+    sfigure(1);
     semilogy(rc, err,'.-', rc, est, '--r')  
     hold on
     ylim([1e-14 inf]);
     
-    figure(2)
+    sfigure(2);
     thiskvot = est'./err;
     plot_idx = thiskvot>0.9;
     plot(rc(plot_idx), thiskvot(plot_idx),'.-')
@@ -89,21 +97,33 @@ end
 plot(rc, rc.^0, '--k')
 
 %%
-figure(1)
+figure(1), publication_fig
 title(['Truncation error of real space part (RMS), \xi = '...
-        sprintf('%g,',xilist) ]);
+        sprintf('%g,',xilist)]);
 xlabel('r_c')
 ylabel('E_{RMS}')
+xlim([min(rc) max(rc)])
 ylim([1e-14 inf]);
 grid on
 
-figure(2)
+figure(2), publication_fig
 leglist={};
 for xi=xilist
     leglist{end+1} = sprintf('\\xi=%g',xi);
 end
 legend(leglist)
+xlim([min(rc) max(rc)])
 ylim([0.9 1.1])
 ylabel('Estimate / Measured (RMS)')
 xlabel('r_c')
 title('Accuracy of real space truncation error estimate')
+
+N=size(f,1)
+box
+
+
+%%
+if false
+write_fig(1,'output/e_rms_rc')
+write_fig(2,'output/e_rms_rc_acc')
+end
