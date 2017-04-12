ee7cc736cca003702d8250d907344ce51d909c6d
Davoud Saffar <davoudss@kth.se>
2017-04-12 15:20:03 +0200

    total runtime in potential: tests

--- SE1P/tests/total_runtime.m
+++ SE1P/tests/total_runtime.m
@@ -1,48 +1,54 @@
+%% figure 8 example 2 in paper 
+%% total runtime in potential 
+%% with 1e-6 accuracy
+
 clear
 rng(1);
 
-N = 20000;
-L = 5;
-box = [L L L];
-[x, f] = vector_system(N, box);
+Llist = 6:14;
+Nlist = 100*Llist.^3;
+Mlist = [46 54 62 64 80 84 92 100 108];
+NLlist = [4 5 6 6 6 7 8 9 10];
 
-opt.M = 46;
-opt.xi = .1;
-opt.P = 16;
-opt.rc = 2.5;
-opt.box = box;
-opt.layers = 0;
-opt.sl = 3;
-opt.nl = 4;
-opt.s0 = 2.5;
+time = [];
+for k=1:numel(Llist)
+    N = Nlist(k);
+    L = Llist(k);
+    box = [L L L];
+    [x, f] = vector_system(N, box);
 
+    opt.M = Mlist(k);
+    opt.xi = 3.5;
+    opt.P = 16;
+    opt.rc = .9;
+    opt.box = box;
+    opt.layers = 5;
+    opt.sl = 3;
+    opt.nl = NLlist(k);
+    opt.s0 = 2.5;
 
-%% Ewald
-disp('One periodic Ewald...')
-[uf tfourier]= se1p_fourier_space_force(x, f, opt);
-[ur treal]= se1p_real_space(1:N,x, f, opt,false);
-tic
-ur3 = SE1P_rsrc_cell_mex(x', f, opt.rc, opt.xi, opt.layers, box);
-toc
-treal
-norm(ur3-ur)/N
-
-% [ur treal]= se1p_real_space_force(1:N,x, f, opt,false);
-% ur3 = SE1P_rsrc_cell_force_mex(x', f, opt.rc, opt.xi, opt.layers,box)';
-% norm(ur3-ur)/N
-return
-ue = uf+ur;
+    
+    %% Ewald
+    disp('One periodic Ewald...')
+    [uf tfourier]= se1p_fourier_space(x, f, opt);
+    tr=tic;
+    ur = SE1P_rsrc_cell_mex(x', f, opt.rc, opt.xi, opt.layers, box);
+    treal=toc(tr);
+    ue = uf+ur;
+    time(end+1) = tfourier.total+treal;
+end
+plot(Nlist,time,'b.')
+hold on
+plot(Nlist,Nlist./Nlist(1)*time(1),'r--')
 
 %% Direct
 % idx = 1:N;
 % disp('Direct sum...')
 % opt.P=24;
-% u1 = se1p_fourier_space_direct_force(idx,x, f, opt, false);
-% u2 = se1p_real_space_force(idx,x, f, opt,true);
-% u3 = se1p_k0_direct_force(idx,x,f,opt,true);
+% u1 = se1p_fourier_space_direct(idx,x, f, opt, false);
+% u2 = se1p_real_space(idx,x, f, opt,true);
+% u3 = se1p_k0_direct(idx,x,f,opt,true);
 % u  = u1+u2+u3;
 
 % ue = ue(idx,:);
 % rms_error = rms(u(:)-ue(:)) / rms(u(:))
-
-time = tfourier.total+treal
